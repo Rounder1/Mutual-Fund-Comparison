f164bad7b40a19d2971502ac696d8e10277e7b51
Shazia Jiwani <jshazia@gmail.com>
2018-05-24 20:37:44 -0400

    added schema and login/register handlebars pages

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,23 @@
 ### Schema
+DROP DATABASE IF EXISTS funds_db;
+CREATE DATABASE funds_db;
+USE funds_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
-
-CREATE TABLE cats
+CREATE TABLE funds
 (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+	fund_id int NOT NULL AUTO_INCREMENT,
+	symbol varchar(10) NOT NULL,
+	expense_ratio DOUBLE ALLOW NULL,
+	PRIMARY KEY (fund_id)
+);
+
+CREATE TABLE funds_history
+(
+	hist_id int NOT NULL AUTO_INCREMENT,
+	close_date DATETIME NOT NULL,
+	close_price int NOT NULL,
+	fund_id int,
+	PRIMARY KEY (hist_id),
+	FOREIGN KEY(fund_id) REFERENCES funds(fund_id)
+
 );
