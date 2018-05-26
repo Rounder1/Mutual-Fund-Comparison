20967065cf20000ee84aad828c0889d470eb6395
Rounder1 <tolerdavid@gmail.com>
2018-05-26 19:40:51 -0400

    updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
-});
 
-// Make connection.
+if(process.env.JAWSDB_URL) {
+    //Access Heroku env for deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ // Set up MySQL connection for local testing.
+    mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "",
+        password: "",
+        database: "funds_db"
+    });
+};
+
+
+// Attempt to make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,6 +26,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
