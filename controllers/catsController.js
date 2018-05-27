22b6f7476cc29c80928bf78ce2c8a0407cfe5bb5
Rounder1 <tolerdavid@gmail.com>
2018-05-27 19:47:59 -0400

    added outside api call

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -1,5 +1,11 @@
 var express = require("express");
 
+// Needed for server side API requests to other websites (and other HTTP requests in general)
+var request = require('request');
+// Import API Keys
+var keys = require("../keys.js");
+var alphaVantageKey = keys.alphaVantage.key;
+
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
@@ -15,6 +21,45 @@ router.get("/", function(req, res) {
     res.render("index", hbsObject);
   });
 });
+
+
+// Get the fund's close history by that fund's symbol
+router.get("/api/newfund/:symbol", function(req, res) {
+  // Get the fund symbol from the http request variable 
+  var fundSymbol = req.params.symbol;
+
+  // Make API call to Alpha Vantage 
+  request("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=" + fundSymbol + "&apikey=" + alphaVantageKey, (error, response, body) => {
+      // Prints an error or returns null if none occur
+      console.log('error:', error);
+      // Prints the response status code 
+      console.log('statusCode:', response && response.statusCode); 
+      // Parses the JSON returned by the API
+      var fundInfo = JSON.parse(body);
+      // Pull the history of the fund by month out of the parsed body object
+      var timeSeries = fundInfo["Monthly Time Series"];
+      // Array that contains useable financial history in decimal form
+      var dateRows = [];
+
+      // loops through each month in the timeSeries object and pushes the result to the dateRows array so that it is easy to work with
+      for (var keys in timeSeries) {
+
+          if (timeSeries[keys]) {
+              var finData = timeSeries[keys];
+              var close = parseFloat(finData["4. close"]);           
+            
+              // pushs the data, only sending the date and close price 
+              dateRows.push({
+                  date: keys,
+                  close
+              });
+          }
+      }
+
+      res.send(dateRows);
+    });
+});
+
 
 router.post("/api/cats", function(req, res) {
   cat.create([
