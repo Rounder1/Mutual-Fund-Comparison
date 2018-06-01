b8b9a77b25f8a6990d8a02b1f114df4f8c707de3
Rodolfo Saez <rsaez@vmware.com>
2018-05-31 23:42:37 -0400

    going to the last working commit

--- public/js/linegraph.js
+++ public/js/linegraph.js
@@ -1,53 +1,31 @@
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01';
+document.addEventListener("DOMContentLoaded", function(event) {
+    fetch(api)
+        .then(function(response) { return response.json(); })
+        .then(function(data) {
+                var parsedData = parseData(data);
+                drawChart(parsedData);
+            })
+            function parseData(data) {
+                var arr = [];
+                for (var i in data.bpi) {
+                    arr.push(
+                        {
+                            date: new Date(i), //date
+                            value: +data.bpi[i] //convert string to number
+                        });
+                }
+                return arr;
+            }
 
-var parseTime = d3.timeParse("%d-%b-%y");
-
-var x = d3.scaleTime()
-    .rangeRound([0, width]);
-
-var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
-
-var line = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
-
-d3.tsv("data.tsv", function(d) {
-    d.date = parseTime(d.date);
-    d.close = +d.close;
-    return d;
-}, function(error, data) {
-    if (error) throw error;
-
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain(d3.extent(data, function(d) { return d.close; }));
-
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .select(".domain")
-        .remove();
-
-    g.append("g")
-        .call(d3.axisLeft(y))
-        .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Price ($)");
-
-    g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 1.5)
-        .attr("d", line);
+            function drawChart(data) {
+                var svgWidth = 600, svgHeight = 400;
+                var margin = {top: 20, right: 20, bottom: 30, left: 50};
+                var width = svgWidth - margin.left - margin.right;
+                var height = svgHeight - margin.top - margin.bottom;
+                var svg = d3.select('svg')
+                    .attr("width", svgWidth)
+                    .attr("height", svgHeight);
+            }
+            //DO SOMETHING WITH DATA
 });
