d4c258e22a8b706847395752f9ef7224458df9a1
Shazia Jiwani <jshazia@gmail.com>
2018-06-02 11:35:09 -0400

    add fund form

--- db/schema.sql
+++ db/schema.sql
@@ -5,19 +5,11 @@ USE funds_db;
 CREATE TABLE funds
 (
 	fund_id int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL,
 	symbol varchar(10) NOT NULL,
 	fund_name varchar(150) NOT NULL,
 	expense_ratio DOUBLE,
 	PRIMARY KEY (fund_id)
 );
 
-CREATE TABLE funds_history
-(
-	hist_id int NOT NULL AUTO_INCREMENT,
-	close_date DATETIME NOT NULL,
-	close_price int NOT NULL,
-	fund_id int,
-	PRIMARY KEY (hist_id),
-	FOREIGN KEY(fund_id) REFERENCES funds(fund_id)
-
-);
+ALTER TABLE funds ADD FOREIGN KEY (id) REFERENCES users(id);
